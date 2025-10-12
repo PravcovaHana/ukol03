d46873e34ea43a8f26a1728246410cdb1bddf60a
pravcovahana <hana.pravcova@post.cz>
2025-10-12 21:17:26 +0200

    ukol03

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,7 +1,6 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
-import cz.czechitas.java2webapps.ukol3.Entity.Vizitka;
-import jakarta.websocket.server.PathParam;
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/cz/czechitas/java2webapps/ukol3/entity/Vizitka.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/entity/Vizitka.java
@@ -1,4 +1,4 @@
-package cz.czechitas.java2webapps.ukol3.Entity;
+package cz.czechitas.java2webapps.ukol3.entity;
 
 public class Vizitka {
 
